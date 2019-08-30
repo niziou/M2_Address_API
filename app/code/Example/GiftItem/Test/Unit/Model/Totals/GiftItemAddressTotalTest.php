51e20b16970f937d627e1b79e0acd7a1f7c76846
Mateusz Niziolek <mateusz.niziolek@gmail.com>
2019-08-30 13:00:07 +0200

    Added first test that checks if Total inherits from right model

--- app/code/Example/GiftItem/Test/Unit/Model/Totals/GiftItemAddressTotalTest.php
+++ app/code/Example/GiftItem/Test/Unit/Model/Totals/GiftItemAddressTotalTest.php
@@ -3,9 +3,13 @@ declare(strict_types=1);
 
 namespace Example\GiftItem\Model\Totals;
 
+use Magento\Quote\Model\Quote\Address\Total\AbstractTotal;
 use PHPUnit\Framework\TestCase;
 
 class GiftItemAddressTotalTest extends TestCase
 {
-
+    public function testInheritsAbstractAdressTotalModel()
+    {
+        $this->assertInstanceOf(AbstractTotal::class, new GiftItemAddressTotal());
+    }
 }
