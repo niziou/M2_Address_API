4b1d4ef2b69b6753ccdc20b96bd5d2c4e24e05fa
mniziolek <mateusz.niziolek@polcode.net>
2019-09-13 17:25:04 +0200

    Added unittests to correct class

--- app/code/Example/GiftItem/Test/Unit/Model/Totals/GiftItemAddressTotalTest.php
+++ app/code/Example/GiftItem/Test/Unit/Model/Totals/GiftItemAddressTotalTest.php
@@ -1,15 +1,53 @@
 <?php
+
 declare(strict_types=1);
 
-namespace Example\GiftItem\Model\Totals;
+namespace Example\GiftItem\Test\Integration\Model\Totals;
 
-use Magento\Quote\Model\Quote\Address\Total\AbstractTotal;
+use Example\GiftItem\Model\GiftItemManager;
+use Example\GiftItem\Model\Totals\GiftItemAddressTotal;
+use Magento\Catalog\Model\Product as ProductModel;
+use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
+use Magento\CatalogInventory\Api\StockRegistryInterface;
+use Magento\Quote\Model\Quote;
+use Magento\Sales\Model\Order\Total\AbstractTotal;
+use Magento\TestFramework\ObjectManager;
 use PHPUnit\Framework\TestCase;
+
+use function array_merge as merge;
 
 class GiftItemAddressTotalTest extends TestCase
 {
-    public function testInheritsAbstractAdressTotalModel()
+    /**
+     * @param float|int $rowTotal
+     * @return Quote\Item\AbstractItem|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createItemWithRowTotal($rowTotal, $baseRowTotal = null): Quote\Item\AbstractItem
+    {
+        $methods = merge(get_class_methods(Quote\Item\AbstractItem::class), ['getRowTotal', 'getBaseRowTotal']);
+        $item = $this->getMockBuilder(Quote\Item\AbstractItem::class)
+            ->disableOriginalConstructor()
+            ->setMethods($methods)
+            ->getMock();
+        $item->method('getRowTotal')->willReturn($rowTotal);
+        $item->method('getBaseRowTotal')->willReturn($baseRowTotal);
+
+        return $item;
+    }
+
+    public function testInheritsAbstractAddressTotalModel()
     {
         $this->assertInstanceOf(AbstractTotal::class, new GiftItemAddressTotal());
+    }
+
+    public function testReturnZeroIfOnlyNonGiftItemIsPassed()
+    {
+        $this->assertSame(0, (new GiftItemAddressTotal())->getGiftItemTotalSum());
+    }
+
+    public function testReturnZeroIfNonGiftItemPassed()
+    {
+        $nonGiftItem = $this->createItemWithRowTotal(10);
+        $this->assertSame(0, (new GiftItemAddressTotal())->getGiftItemTotalSum($nonGiftItem));
     }
 }
