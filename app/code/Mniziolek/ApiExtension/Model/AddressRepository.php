325be0305e43f728440a69d8aeb22d25d67852a3
Mateusz Niziolek <mateusz.niziolek@gmail.com>
2019-11-27 10:13:48 +0100

    Added DI search and get interfaces

--- app/code/Mniziolek/ApiExtension/Model/AddressRepository.php
+++ app/code/Mniziolek/ApiExtension/Model/AddressRepository.php
@@ -5,24 +5,31 @@ namespace Mniziolek\ApiExtension\Model;
 
 use Magento\Customer\Api\Data\AddressInterface;
 use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Mniziolek\ApiExtension\Api\Customer\AddressManagementInterface;
-use Mniziolek\ApiExtension\Model\Address\Query\Get;
+use Mniziolek\ApiExtension\Model\Address\Query\GetInterface;
+use Mniziolek\ApiExtension\Model\Address\Query\SearchInterface;
 
 class AddressRepository implements AddressManagementInterface
 {
     private $getQuery;
+    private $searchQuery;
     public function __construct(
-        Get $getQuery
+        GetInterface $getQuery,
+        SearchInterface $searchQuery
     )
     {
-
+        $this->getQuery = $getQuery;
+        $this->searchQuery = $searchQuery;
     }
+
     /**
      * Get Address Entity By ID
      *
      * @param int $customerId
      * @param int $addressId
      * @return AddressInterface
+     * @throws NoSuchEntityException
      */
     public function get(int $customerId, int $addressId): AddressInterface
     {
