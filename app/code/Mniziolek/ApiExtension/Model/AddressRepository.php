1774ebe1b3073e74a29b82d21ae915616f2ccab5
Mateusz Niziolek <mateusz.niziolek@gmail.com>
2019-11-27 13:46:50 +0100

    Added comments and fixed commands implementations - they should return void because they are writing to database.

--- app/code/Mniziolek/ApiExtension/Model/AddressRepository.php
+++ app/code/Mniziolek/ApiExtension/Model/AddressRepository.php
@@ -9,21 +9,52 @@ namespace Mniziolek\ApiExtension\Model;
 
 use Magento\Customer\Api\Data\AddressInterface;
 use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
-use Mniziolek\ApiExtension\Api\Customer\AddressManagementInterface;
+use Mniziolek\ApiExtension\Api\CustomerAddressRepositoryInterface;
+use Magento\Customer\Api\Data\AddressSearchResultsInterface;
 use Mniziolek\ApiExtension\Model\Address\Query\GetInterface;
 use Mniziolek\ApiExtension\Model\Address\Command\CreateInterface;
 use Mniziolek\ApiExtension\Model\Address\Query\SearchInterface;
 use Mniziolek\ApiExtension\Model\Address\Command\UpdateInterface;
 use Mniziolek\ApiExtension\Model\Address\Command\DeleteInterface;
 
+/**
+ * Class AddressRepository
+ * @package Mniziolek\ApiExtension\Model
+ */
 class AddressRepository implements CustomerAddressRepositoryInterface
 {
+    /**
+     * @var GetInterface
+     */
     private $getQuery;
+    /**
+     * @var SearchInterface
+     */
     private $searchQuery;
+    /**
+     * @var CreateInterface
+     */
     private $createCommand;
+    /**
+     * @var UpdateInterface
+     */
     private $updateCommand;
+    /**
+     * @var DeleteInterface
+     */
     private $deleteCommand;
+
+    /**
+     * AddressRepository constructor.
+     * @param GetInterface $getQuery
+     * @param SearchInterface $searchQuery
+     * @param CreateInterface $createCommand
+     * @param UpdateInterface $updateCommand
+     * @param DeleteInterface $deleteCommand
+     */
     public function __construct(
         GetInterface $getQuery,
         SearchInterface $searchQuery,
@@ -40,8 +71,8 @@ class AddressRepository implements CustomerAddressRepositoryInterface
     }
 
     /**
-     * Get Address Entity By ID
-     *
+     * Get Query reads the Address Entity By ID and checks if address belongs to customer
+     * @todo refactor: validation should be out
      * @param int $customerId
      * @param int $addressId
      * @return AddressInterface
@@ -52,16 +83,37 @@ class AddressRepository implements CustomerAddressRepositoryInterface
         return $this->getQuery->execute($customerId, $addressId);
     }
 
-    public function search(int $customerId, SearchCriteriaInterface $searchCriteria = null)
+    /**
+     * Search Query reads the AddressResultCollection by the given SearchCriteria
+     * @param int $customerId
+     * @param SearchCriteriaInterface|null $searchCriteria
+     * @return AddressSearchResultsInterface
+     */
+    public function search(int $customerId, SearchCriteriaInterface $searchCriteria = null): AddressSearchResultsInterface
     {
         return $this->searchQuery->execute($customerId, $searchCriteria);
     }
 
+    /**
+     * Create new address for given customer
+     * @param int $customerId
+     * @param AddressInterface $addressData
+     * @throws InputException
+     * @throws LocalizedException
+     */
     public function create(int $customerId, AddressInterface $addressData)
     {
-        return $this->createCommand->execute($customerId, $addressData);
+        $this->createCommand->execute($customerId, $addressData);
+        return;
     }
 
+    /**
+     * Delete customer address by entity ID and checks if customer owns that address
+     * @todo refactor: validation should be out
+     * @param int $customerId
+     * @param int $addressId
+     * @throws NoSuchEntityException
+     */
     public function delete(int $customerId, int $addressId)
     {
         $address = $this->get($customerId,$addressId);
@@ -69,8 +121,16 @@ class AddressRepository implements CustomerAddressRepositoryInterface
         return;
     }
 
+    /**
+     * Update customer address by entity Id
+     * @param int $customerId
+     * @param int $addressId
+     * @param AddressInterface $addressData
+     * @throws LocalizedException
+     */
     public function update(int $customerId, int $addressId, AddressInterface $addressData)
     {
-        return $this->updateCommand->execute($customerId, $addressId, $addressData);
+        $this->updateCommand->execute($customerId, $addressId, $addressData);
+        return;
     }
 }
